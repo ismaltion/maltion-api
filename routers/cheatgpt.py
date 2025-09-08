15e10fc8c57a1a810c18baed8cb3aed2cc1f378a
ismaltion <ismaelfjgd@gmail.com>
2025-09-09 00:41:42 +0200

    disabled ethics

--- routers/cheatgpt.py
+++ routers/cheatgpt.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, APIRouter
 from pydantic import BaseModel
 from db import get_google_api_key
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 genai.configure(api_key=get_google_api_key())
 model = genai.GenerativeModel("gemini-2.0-flash")
@@ -21,7 +22,24 @@ async def chat_endpoint(request: ChatRequest):
     try:
         history_as_dicts = [msg.model_dump() for msg in request.history]
 
-        chat = model.start_chat(history=history_as_dicts)
+        chat = model.start_chat(history=history_as_dicts, safety_settings=[
+        {
+            "category": HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            "threshold": HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+            "category": HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            "threshold": HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+            "category": HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            "threshold": HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+            "category": HarmCategory.HARM_CATEGORY_HARASSMENT,
+            "threshold": HarmBlockThreshold.BLOCK_NONE,
+        },
+    ])
 
         response = chat.send_message(request.prompt)
 
