667d62ed10fe070608e10e8d73556d967dacace8
ismaltion <ismaelfjgd@gmail.com>
2025-09-19 22:57:15 +0200

    fixed migration

--- routers/user.py
+++ routers/user.py
@@ -76,7 +76,7 @@ def mclient_migration(
     issue_flag = False
 
     # start of extensive checks
-    if len(username) < 32 or len(username) > 60:
+    if len(username) < 3 or len(username) > 60:
         return JSONResponse(status_code=400, content={"detail": "Username must be between 3 and 60 characters long."})
     if not re.match(USERNAME_REGEX, username, re.IGNORECASE):
         return JSONResponse(status_code=400, content={"detail": "Invalid username. Only letters, numbers and hyphens are allowed."})
